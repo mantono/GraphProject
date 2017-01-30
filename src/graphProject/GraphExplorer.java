9b3bda02a138a079711aa963888b2e31ce76bd1d
Anton Österberg <anton.osterberg@gmail.com>
2017-01-31 00:03:07 +0100

    Interface GraphExplorer now has a method for telling whether a graph is connected or not, but it is not properly implemented yet by any class.

--- src/graphProject/GraphExplorer.java
+++ src/graphProject/GraphExplorer.java
@@ -7,6 +7,7 @@ public interface GraphExplorer<T>
 	List<T> depthFirstSearch(T start, T end);
 	List<T> breadthFirstSearch(T start, T end);
 	boolean hasPath(T start, T end);
+	boolean isConnected();
 	int getTotalWeight(T start, T end);
 	List<Edge<T>> getShortestPath(T start, T end);
 	List<Edge<T>> getPathAllNodes();
