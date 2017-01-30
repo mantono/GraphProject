04dc6cf14773f5088f84101df5ef906227c6aff9
Anton Österberg <anton.osterberg@gmail.com>
2017-01-30 23:57:56 +0100

    Graph interface now contains method for checking whether a node is connected or not (has any edges).

--- src/graphProject/Graph.java
+++ src/graphProject/Graph.java
@@ -14,6 +14,7 @@ public interface Graph<T> extends Collection<T>, RandomAccess<T>
 	boolean contains(Object data);
 	int edgeSize(T data);
 	boolean isDirected();
+	boolean isConnected(T node);
 	boolean isConnected(T start, T end);
 	boolean connect(T start, T end, double weight);
 	boolean disconnect(T start, T end);
